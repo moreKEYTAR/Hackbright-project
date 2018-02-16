d43e4ccb06ac6515e70755e432a1125441e0d37a
vagrant <vagrant@vagrant.vm>
2018-02-16 17:20:48 +0000

    Completed js for new team form; committing before rewriting /new-team route

--- static/dashboard.js
+++ static/dashboard.js
@@ -1,15 +1,37 @@
 "use strict";
 
+
+function makeTeamElements() {
+
+
+
+
 $('#make-team').on('click', function (evt) {
     let newTeamName = $("#new-team-name").val();
     let newTeamDesc = $("#new-team-desc").val();
-    $.post("/new-team", {"name": newTeamName, "desc": newTeamDesc}, function (results) {
-        //assume results = {teamId: xxxxxx}
+    $.post("/new-team", {"name": newTeamName, "desc": newTeamDesc}, 
+        function (results) {
+        // results = {teamId: xxxxxx}
+            // Should I put these into a separate function?
         let div = $("<div>");
         let form = $("<form>");  // makes a form element, still unattached
-        form.attr({"action": "/view-team", "method": "GET"});
-        div.append(form);   // makes form a child of the div...appending to the end of the child list
-        
+            form.attr({"action": "/view-team", "method": "GET"});
+        let inputTeamId = $("<input>");
+            inputTeamId.attr({"type": "hidden", "name": "team", 
+                              "value": results.teamId});
+        let inputSubmit = $("<input>");
+            inputSubmit.attr({"type": "submit", "value": newTeamName});
+        let descPara = $("<p>");
+            descPara.attr({"class": "desc"});
+            desc.Para.html(newTeamDesc);
+
+        // Connecting our elements to each other and dashboard.html tree
+        form.append(inputTeamId);
+        form.append(inputSubmit);
+        form.append(descPara);
+        div.append(form);
+        $('#joined-teams').append(div);
+        });
     });
 }
 );
